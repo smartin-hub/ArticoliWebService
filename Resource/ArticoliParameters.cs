96bdde04328afc104c925a99914e23456cfbe5b4
smartin-hub <smartin@invallee.it>
2021-01-29 09:46:37 +0100

    commit di prova

--- Resource/ArticoliParameters.cs
+++ Resource/ArticoliParameters.cs
@@ -5,5 +5,6 @@ namespace ArticoliWebService.Resource
     public class ArticoliParameters {
         public string IdCategoria {get; set;} = "";
         public decimal Prezzo {get; set;} = 0;
+        
     }
 }
